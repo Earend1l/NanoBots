723033b3d7b0c1df8151170b97dc2733cf3718fa
Jason <jason.gaudinet@gmail.com>
2015-11-14 23:24:04 +0100

    Pink is not gay
    Changed the name of the textures to "texture.png" so it's easier to
    edit.

--- src/GameClient/ResourcesManager.cpp
+++ src/GameClient/ResourcesManager.cpp
@@ -22,7 +22,7 @@ sf::Texture& ResourcesManager::getTexture(std::string entityName)
 
     if (it == m_textures.end())
     {
-        std::string path = "data/entities/"+entityName+"/texture";
+        std::string path = "data/entities/"+entityName+"/texture.png";
         m_textures.insert(std::make_pair<std::string,sf::Texture>(std::string(entityName),sf::Texture()));
         m_textures[entityName].loadFromFile(path);
     }
